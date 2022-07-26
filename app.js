79bdc6d7f2d4a78e6cf1d3cff05a1537022161e4
jarrellrn <jarrellrn@gmail.com>
2022-07-26 10:14:43 -0500

    very unfortunately added real functionality to get user button and got commented beebo's existence out

--- app.js
+++ app.js
@@ -13,12 +13,22 @@ const app = Vue.createApp({
     // getUser() {
     //   console.log(this.firstName + " is a good boy")
     // }
-    getUser() {
-      this.firstName = "Beebo",
-      this.lastName = "the Not Smuggler",
-      this.email = "beebo@legitbusiness.com"
-      this.gender = "thief",
-      this.picture = "./assets/beebo.jpg"
+    // getUser() {
+    //   this.firstName = "Beebo",
+    //   this.lastName = "the Not Smuggler",
+    //   this.email = "beebo@legitbusiness.com"
+    //   this.gender = "thief",
+    //   this.picture = "./assets/beebo.jpg"
+    // }
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api")
+      const { results } = await res.json()
+
+      this.firstName = results[0].name.first 
+      this.lastName = results[0].name.last 
+      this.email = results[0].email
+      this.gender = results[0].gender
+      this.picture = results[0].picture.large
     }
   }
 })
